7672b20c34c80209eb1e6f94c9792ed4862a64de
Juan Pablo Libares de Lamadrid <juanchostrat@gmail.com>
2024-02-20 19:24:36 -0300

    asd

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://juanlibares.github.io/portfolio",
+  base:"https://juanlibares.github.io/Portfolio",
   plugins: [react()],
 })
